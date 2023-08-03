78587e25556d3bdd7ac1b190824410d6f3019629
fujiroh <kentarof319@gmail.com>
2023-08-03 12:11:35 +0900

    Task:ダミーファイルのファイルパスを設定

--- RoslynAnalyzerApp/Program.cs
+++ RoslynAnalyzerApp/Program.cs
@@ -1,9 +1,14 @@
-﻿namespace RoslynAnalyzerApp
+﻿using System.IO;
+
+namespace RoslynAnalyzerApp
 {
     internal class Program
     {
+        private static readonly string DummySolutionPath = @"C:\git\RoslynAnalyzerApp\SampleDesktopApp";
+
         public static void Main(string[] args)
         {
+            var files = Directory.EnumerateFiles(DummySolutionPath, "*.cs");
         }
     }
 }
